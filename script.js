ea4f4293c9dd6bb564a76831e5f620f05ff76cd8
Daniil Stasulevich <alexgrist133@gmail.com>
2023-03-13 13:09:17 +0300

    refactor: remove spaces, add extra line after variables

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
 const crypto = require('crypto');
 const readline = require('readline');
+
 const moves = process.argv.slice(2);
 
-class Game{
+class Game {
     constructor(moves) {
         this.moves = [...moves];
     }
 
-    makeRandomTurn(){
+    makeRandomTurn() {
         const randomIndex = Math.floor(Math.random() * this.moves.length)
         return this.moves[randomIndex];
     }
 
-    getGameResult(userTurn, computerTurn,winMessage = 'You win!',loseMessage='Computer wins!'){
+    getGameResult(userTurn, computerTurn, winMessage = 'You win!', loseMessage = 'Computer wins!') {
         const diff = (computerTurn - userTurn + this.moves.length) % this.moves.length;
-        if(diff === 0){
+        if (diff === 0) {
             return 'Draw';
-        } else if(diff <= moves.length / 2){
+        } else if (diff <= moves.length / 2) {
             return winMessage;
-        }else{
+        } else {
             return loseMessage;
         }
     }
-
 }
 
-class SecretKey{
-    generateRandomKey(){
+class SecretKey {
+    generateRandomKey() {
         return crypto.randomBytes(32).toString('hex');
     }
 }
 
-class Hmac{
-    constructor(turn,key) {
+class Hmac {
+    constructor(turn, key) {
         this.turn = turn;
         this.key = key;
     }
-    createHmac(){
-        return crypto.createHmac('sha3-256',this.key).update(this.turn).digest('hex');
-    }
 
+    createHmac() {
+        return crypto.createHmac('sha3-256', this.key).update(this.turn).digest('hex');
+    }
 }
 
-class Table{
-    showTable(moves){
+class Table {
+    showTable(moves) {
         const newGame = new Game(moves);
-        const matrix =[];
+        const matrix = [];
         for (let i = 0; i < moves.length; i++) {
             const row = {};
             for (let j = 0; j < moves.length; j++) {
@@ -53,31 +53,30 @@ class Table{
             }
             matrix.push(row);
         }
-        const newMatrix = matrix.map((obj,index) => ({" ":moves[index], ...obj}));
+        const newMatrix = matrix.map((obj, index) => ({" ": moves[index], ...obj}));
         console.table(newMatrix);
     }
 }
 
-
-function startGame(){
-    if(hasDuplicates(moves)){
+function startGame() {
+    if (hasDuplicates(moves)) {
         console.log('Values should not be the same');
-    }else if(moves.length < 3){
+    } else if (moves.length < 3) {
         console.log('Number of arguments must be more than 2. Example: rock paper scissors');
-    }else if(moves.length%2 === 0){
-        console.log('Number of arguments must be odd(>2). Example: rock paper scissors');
-    }else{
+    } else if (moves.length % 2 === 0) {
+        console.log('Number of arguments must be odd(3 or 5, etc.). Example: rock paper scissors');
+    } else {
         const newKey = new SecretKey();
         const key = newKey.generateRandomKey();
         const newGame = new Game(moves);
         const randomTurn = newGame.makeRandomTurn();
-        const newHmac = new Hmac(randomTurn,key);
+        const newHmac = new Hmac(randomTurn, key);
         const hmac = newHmac.createHmac();
 
         console.log(`HMAC: ${hmac}`);
         console.log('Available moves:');
-        moves.forEach((item,i)=>{
-            console.log(`${i+1} - ${item}`);
+        moves.forEach((item, i) => {
+            console.log(`${i + 1} - ${item}`);
         })
         console.log('0 - exit\n? - help');
 
@@ -86,38 +85,37 @@ function startGame(){
             output: process.stdout
         });
 
-        function askForMove(){
+        function askForMove() {
             rl.question('Enter your move: ', (number) => {
-                const move = moves[Number(number-1)];
-                if(number === '0') {
+
+                const move = moves[Number(number - 1)];
+
+                if (number === '0') {
                     rl.close();
-                }else if(number === '?'){
+                } else if (number === '?') {
                     const newTable = new Table();
                     newTable.showTable(moves);
                     askForMove();
-                }
-                else if(!move){
+                } else if (!move) {
                     console.log(`You must input correct value`);
                     askForMove();
-                }
-                else{
+                } else {
                     console.log(`Your move: ${move}`);
                     console.log(`Computer move: ${randomTurn}`);
-                    console.log(newGame.getGameResult(+number,moves.indexOf(randomTurn) + 1))
+                    console.log(newGame.getGameResult(+number, moves.indexOf(randomTurn) + 1))
                     console.log(`HMAC key: ${key}`);
                     rl.close();
                 }
             });
         }
-
         askForMove();
     }
 }
 
-function hasDuplicates(arr){
-    for(let i =0; i<arr.length; i++){
-        for(let j = i+1; j<arr.length; j++){
-            if(arr[i] === arr[j])
+function hasDuplicates(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] === arr[j])
                 return true;
         }
     }
@@ -125,4 +123,3 @@ function hasDuplicates(arr){
 }
 
 startGame();
-
